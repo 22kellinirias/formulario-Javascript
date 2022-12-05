f46886bc7cc4067f650cca81129a37b297aed61d
kellinirias <meliirias95@gmail.com>
2022-12-05 12:53:59 -0600

    trabajando en index y css

---
+++ index.js
@@ -0,0 +1,5 @@
+//Obtener informacion de la forma
+const nombre = document.getElementById("nombre");
+const correo  = document.getElementById("email");
+const descripcion  = document.getElementById("decripcion")
+const  = document.getElementById("email")
